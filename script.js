88c5da49592edf42b1007f6103bf53c9fe600709
Nicholas McCullum <nicholasmccullum@gmail.com>
2019-09-24 10:00:35 -0300

    Link to my own .json file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const endpoint = 'https://raw.githubusercontent.com/nicholasmccullum/us-cities-by-population/master/us.json';
 
 const cities = [];
 fetch(endpoint)
@@ -7,7 +7,6 @@ fetch(endpoint)
 
 function findMatches(wordToMatch, cities) {
   return cities.filter(place => {
-    // here we need to figure out if the city or state matches what was searched
     const regex = new RegExp(wordToMatch, 'gi');
     return place.city.match(regex) || place.state.match(regex)
   });
